c7c76580f3fbf37cb03c3fdf4de1f26d9b179b04
Bruno Gelatti <bgelatti@gmail.com>
2017-06-05 19:01:30 -0300

    criando o projeto

--- comandosbanco.sql
+++ comandosbanco.sql
@@ -5,3 +5,13 @@ create table produto (
 	nome varchar(255),
 	preco double
 );
+
+create table pessoa (
+	id int not null auto_increment primary key,
+	nome varchar(255),
+	cpf_cnpj varchar(20),
+	rg_ie varchar(20),
+	dt_nascimento varchar(30),
+	endereco varchar(255),
+	telefone varchar(20)
+);
